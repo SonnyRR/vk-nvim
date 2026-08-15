a07ae48c8fcc3099fbf40f40385bc9258f6337ff
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2026-08-15 16:17:35 +0300

    Move neotest testrunner config section

--- lua/plugins/dotnet.lua
+++ lua/plugins/dotnet.lua
@@ -6,9 +6,6 @@ local M = {
       require('easy-dotnet').setup {
         -- NetCoreDbg is now available OOB w/ zero click configuration in easy-dotnet.
         -- However, I will continue to manage it through Mason.
-        test_runner = {
-          neotest_integration = true,
-        },
         debugger = {
           bin_path = (function()
             local is_windows = vim.uv.os_uname().sysname == 'Windows_NT'
@@ -51,6 +48,9 @@ local M = {
               },
             },
           },
+        },
+        test_runner = {
+          neotest_integration = true,
         },
         -- Uncomment in case you need to debug issues.
         -- server = {
